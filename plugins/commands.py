0aec0dce84fd5296d3fe3fbb7540c5dd1ab46424
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2023-11-08 18:27:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,7 +36,7 @@ async def start(client, message):
             InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ ➕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("Sᴇᴀʀᴄʜ ☘️", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🎬", url="https://t.me/ONHIT_CHAT")
+            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🔥", url="https://t.me/ONHIT_EHI")
             ],[
             InlineKeyboardButton("Hᴇʟᴩ 🕸🔥", callback_data="help"),
             InlineKeyboardButton("Aʙᴏᴜᴛ ⚡", callback_data="about"),
@@ -77,7 +77,7 @@ async def start(client, message):
             InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ ➕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("Sᴇᴀʀᴄʜ ☘️", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🎬", url="https://t.me/ONHIT_EHI")
+            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🔥", url="https://t.me/ONHIT_EHI")
             ],[
             InlineKeyboardButton("Hᴇʟᴩ 🕸🔥", callback_data="help"),
             InlineKeyboardButton("Aʙᴏᴜᴛ ⚡", callback_data="about"),
