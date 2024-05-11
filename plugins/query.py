172431294c60d626415fe72d669f4f0e063bcd57
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-05-11 22:41:52 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -431,15 +431,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton("➕️ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Cʜᴀᴛ ➕", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
-            InlineKeyboardButton("Sᴇᴀʀᴄʜ ♂️", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("Cʜᴀɴɴᴇʟ 🔥", url="https://t.me/FulokeOraginal")
-            ],[
-            InlineKeyboardButton("Hᴇʟᴩ 🌸", callback_data="help"),
+            InlineKeyboardButton("Hᴇʟᴩ 🕸🔥", callback_data="help"),
             InlineKeyboardButton("Aʙᴏᴜᴛ ⚡", callback_data="about")
             ],[
-            InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ 🌐", url="https://t.me/KaluKumaraya0"),
-            InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ 🎀", url="https://t.me/FulokeOraginal")
-            ],[  
+            InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ 🍁", url="https://t.me/infinityx_LK")
+            ],[          
             InlineKeyboardButton('❌ Cʟᴏꜱᴇ ❌', callback_data='close_data')
         ]]
         await query.edit_message_media(InputMediaPhoto(random.choice(PICS), START_MESSAGE.format(user=query.from_user.mention, bot=client.mention), enums.ParseMode.HTML), reply_markup=InlineKeyboardMarkup(buttons))
