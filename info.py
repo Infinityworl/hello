92a633736fa6bfe623eb6f1313f21c9196aeeb26
Infinityworl <138610383+Infinityworl@users.noreply.github.com>
2024-05-31 17:18:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,6 +18,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 PICS = (environ.get('PICS' ,'https://te.legra.ph/file/cbb98a1184c78767f44d9.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
 UPTIME = time.time()
 
 # Admins, Channels & Users
